e6e382de75865f3b2027c218de7abf4908c55698
Paloma Maquez <maquez.paloma@gmail.com>
2022-08-22 18:32:18 -0300

    class fixed

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,7 +1,7 @@
 const CartWidget = () => {
     return(
     <div className='cart'>
-        <img src={require('../assets/002-shopping-bag-1.png')} class="cart-pic" alt="cart"/>
+        <img src={require('../assets/002-shopping-bag-1.png')} className="cart-pic" alt="cart"/>
         <span className="item__Total">0</span>
     </div>
     )
